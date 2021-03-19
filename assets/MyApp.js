f0aec7f37bf6be40e9b9cc838a61ea16ed6deab1
root <grzegorzchrymecki@php-developer.ie>
2021-03-19 12:33:42 +0000

    /api/users added

--- assets/MyApp.js
+++ assets/MyApp.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Home from './components/Home/Home';
 import Cart from './components/Cart/Cart';
+import Login from './components/Login/Login';
 
 import './styles/myStyles.scss';
 
@@ -15,6 +16,7 @@ const MyApp = () => {
                 <Nav/>
                 <Route path="/" component={Home} exact/>
                 <Route path="/cart" component={Cart}/>
+                <Route path="/login" component={Login}/>
             </>
         </Router>
 
---
+++ assets/components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+const Login = (props) => {
+    return (
+        <>
+            <div>Login</div>
+        </>
+    );
+}
+export default Login;
