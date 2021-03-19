54cdd16f5c5312a134a67487ce7cce0a83dd6917
root <grzegorzchrymecki@php-developer.ie>
2021-03-19 12:46:09 +0000

    ok

--- assets/components/Nav/Nav.js
+++ assets/components/Nav/Nav.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import { NavLink } from 'react-router-dom';
 
+import './Nav.scss';
+
 const Nav = (props) => {
     return (
         <>
-            <NavLink to="/">home</NavLink>
-            <NavLink to="/cart">cart</NavLink>
+            <nav className="Navigation">
+            <NavLink className="Navigation__link" to="/" exact>home</NavLink>
+            <NavLink className="Navigation__link" to="/cart">cart</NavLink>
+            <NavLink className="Navigation__link" to="/login">login</NavLink>
+        </nav>
         </>
 
 
