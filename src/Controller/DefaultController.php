bf0aed2fade0adf548a2557bd3542dc8d9989035
root <grzegorzchrymecki@php-developer.ie>
2021-03-19 13:57:27 +0000

    ok

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -435,8 +437,10 @@ class DefaultController extends AbstractController
     /**
      * @Route("/api/login", name="api_login", methods={"POST"})
      */
-    public function generateToken():JsonResponse
+    public function generateToken(Request $request, UserService $userService):JsonResponse
     {
+        $userService->verifyUser($request);
+
         return new JsonResponse('ok');
     }
 }
---
+++ src/Service/UserService.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Service;
+
+
+use Symfony\Component\HttpFoundation\Request;
+
+class UserService
+{
+    public function verifyUser(Request $request)
+    {
+
+    }
+}
