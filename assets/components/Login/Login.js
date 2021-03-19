78fb5f538a895dd6090a1025a2d216ad9a3d5da5
root <grzegorzchrymecki@php-developer.ie>
2021-03-19 14:11:28 +0000

    ok

--- assets/components/Login/Login.js
+++ assets/components/Login/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 
+import './Login.scss';
+
 const Login = (props) => {
     return (
         <>
-            <div>Login</div>
+            <div className={'LoginForm'}>
+                <label className={'LoginForm__label'} htmlFor={'email'}>Email</label>
+                <input className={'LoginForm__input'} id={'email'} type="email"/>
+                <label className={'LoginForm__label'} htmlFor={'password'}>Password</label>
+                <input className={'LoginForm__input'} id={'password'} type="password"/>
+                <input className={'LoginForm__btn'} id={'btn'} value={'Log In'} type="submit"/>
+            </div>
         </>
     );
 }
