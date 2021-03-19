0f7b07b75ba92effc6db6f6c39e047389ce53461
root <grzegorzchrymecki@php-developer.ie>
2021-03-19 13:17:51 +0000

    ok

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -24,9 +24,30 @@ class DefaultController extends AbstractController
     public function getUsers():JsonResponse
     {
 
+        $products = [
+            [
+                'id'=>1,
+                'name'=>'',
+                'price'=>12.99,
+                'inStock'=> 6,
+                'rating'=>3.5,
+                'numRating'=>14,
+                'colors'=>[
+                    '#000',
+                    '#f00',
+                    '#0f0',
+                    '#00f',
+                    '#fff'
+                ],
+                'deliveryIn'=>2,
+                'img'=>'/images/',
+                'description'=>'Praesent sodales vulputate sem, eu porttitor odio aliquet eu. Integer ut sapien a enim commodo tempus et a mauris. Fusce metus erat, faucibus et lobortis a, molestie vitae erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque sit amet viverra purus. Nam vitae urna vel libero euismod auctor. Cras sit amet lacus tortor. Suspendisse potenti. Suspendisse potenti. Integer tincidunt accumsan mollis.',
 
+            ],
 
-        return new JsonResponse('ok');
+        ];
+
+        return new JsonResponse($products);
 
     }
 }
