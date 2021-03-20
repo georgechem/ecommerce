62178a827d8f159118cff19b8d6a1c9f4292bef8
root <grzegorzchrymecki@php-developer.ie>
2021-03-20 12:02:25 +0000

    login and logout fully operational

--- assets/components/Card/Card.js
+++ assets/components/Card/Card.js
@@ -8,13 +8,13 @@ const Card = (props) => {
             <div className="Card__item">
                 <div className="Card__item__box">
                     <img className="Card__item__img" src={props.product.img} alt="product image"/>
-                    <div className="Card__item__">{props.product.name}</div>
-                    <div>{props.product.price}</div>
-                    <div>{props.product.inStock}</div>
-                    <div>{props.product.rating}</div>
-                    <div>{props.product.numRating}</div>
-                    <div>{props.product.colors}</div>
-                    <div>{props.product.deliveryIn}</div>
+                    <div className="Card__item__name">{props.product.name}</div>
+                    <div className="Card__item__price">EUR: {props.product.price}</div>
+                    <div className="Card__item__">{props.product.inStock}</div>
+                    <div className="Card__item__">{props.product.rating}</div>
+                    <div className="Card__item__">{props.product.numRating}</div>
+                    <div className="Card__item__">{props.product.colors}</div>
+                    <div className="Card__item__">{props.product.deliveryIn}</div>
                 </div>
             </div>
     );
