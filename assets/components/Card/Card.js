bdec574b140d8b1692949b78dd31ec357e5a2c1f
root <grzegorzchrymecki@php-developer.ie>
2021-03-20 11:55:28 +0000

    login and logout fully operational

--- assets/components/Card/Card.js
+++ assets/components/Card/Card.js
@@ -5,9 +5,18 @@ import './Card.scss';
 
 const Card = (props) => {
     return (
-        <>
-            <div>Card</div>
-        </>
+            <div className="Card__item">
+                <div className="Card__item__box">
+                    <img className="Card__item__img" src={props.product.img} alt="product image"/>
+                    <div className="Card__item__">{props.product.name}</div>
+                    <div>{props.product.price}</div>
+                    <div>{props.product.inStock}</div>
+                    <div>{props.product.rating}</div>
+                    <div>{props.product.numRating}</div>
+                    <div>{props.product.colors}</div>
+                    <div>{props.product.deliveryIn}</div>
+                </div>
+            </div>
     );
 }
 export default Card;
