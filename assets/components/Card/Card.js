9763d026b27890e726cc071456dfae5524a22856
root <grzegorzchrymecki@php-developer.ie>
2021-03-20 12:08:58 +0000

    login and logout fully operational

--- assets/components/Card/Card.js
+++ assets/components/Card/Card.js
@@ -4,6 +4,11 @@ import {useState, useEffect} from 'react';
 import './Card.scss';
 
 const Card = (props) => {
+
+    const fullStar = <i className="fas fa-star"/>;
+    const halfStar = <i className="fas fa-star-half-alt"/>;
+    const emptyStar = <i className="far fa-star"/>;
+
     return (
             <div className="Card__item">
                 <div className="Card__item__box">
@@ -11,7 +16,11 @@ const Card = (props) => {
                     <div className="Card__item__name">{props.product.name}</div>
                     <div className="Card__item__price">EUR: {props.product.price}</div>
                     <div className="Card__item__">{props.product.inStock}</div>
-                    <div className="Card__item__">{props.product.rating}</div>
+                    <div className="Card__item__star">
+                        {props.product.rating > 2 ? 'B' : 'S'}
+                        <span></span>
+
+                    </div>
                     <div className="Card__item__">{props.product.numRating}</div>
                     <div className="Card__item__">{props.product.colors}</div>
                     <div className="Card__item__">{props.product.deliveryIn}</div>
