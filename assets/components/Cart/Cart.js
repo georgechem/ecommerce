f5ab89b079da84310ff2fdf53236c64b302b81a4
root <grzegorzchrymecki@php-developer.ie>
2021-03-21 10:47:02 +0000

    cart add and handle duplicates working

--- assets/components/Cart/Cart.js
+++ assets/components/Cart/Cart.js
@@ -10,6 +10,17 @@ props.setUserCart
 const Cart = (props) => {
     const [inCart, setInCart] = useState(null);
 
+    const getTotal = () => {
+        let total = 0.0;
+        props.userCart.items.forEach(product=>{
+           let amount = parseInt(product.amount);
+           let price = parseFloat(product.price);
+           total += amount * price;
+        });
+
+        return total.toFixed(2);
+    }
+
     useEffect(() => {
         const itemsInCart = [];
         props.userCart.items.forEach((product, key)=>{
@@ -23,6 +34,10 @@ const Cart = (props) => {
     return (
         <div className="CartContainer">
             {inCart !== null ? inCart : null}
+            <div className="Total">
+                <div className="Total__total">Total</div>
+                <div className="Total__price">EUR: {getTotal()}</div>
+            </div>
         </div>
     );
 }
