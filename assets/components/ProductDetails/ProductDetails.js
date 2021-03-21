1f1ba7c8fc97bc1b50e8e76e6eaa3916e43947d4
root <grzegorzchrymecki@php-developer.ie>
2021-03-21 17:00:10 +0000

    cart add and handle duplicates working

--- assets/components/ProductDetails/ProductDetails.js
+++ assets/components/ProductDetails/ProductDetails.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 
 import {useState, useEffect} from 'react';
+
+import './ProductDetails.scss';
 
 const ProductDetails = (props) => {
     return (
         <>
-            <div>ProductDetails{console.log(props.currentProduct)}</div>
+            <div>
+                ProductDetails{console.log(props.currentProduct)}
+            </div>
+
+            <div className="Link">
+                <Link className="Link__home" to="/">Home</Link>
+            </div>
+
         </>
     );
 }
